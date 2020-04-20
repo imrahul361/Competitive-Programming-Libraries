a9709ad5648d67f35ba1e08de0ad4ba9e036af9a
Rahul Karmakar <36432937+imrahul361@users.noreply.github.com>
2020-04-21 05:03:03 +0530

    Refactoring template.cpp

--- template.cpp
+++ template.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
-using namespace std;
 
+#define FAST_IO  ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define mp make_pair 
+#define pb push_back
+#define eb emplace_back
+#define MOD 1000000007
+#define MAX 1e9
+#define MIN -1e9
+#define PQ(type) priority_queue<type>
+#define PQD(type) priority_queue<type,vector<type>,greater<type> >
+#define ITR :: iterator it
+#define t int t; cin>>t; while(t--)
+#define TR(container,it) for(typeof(container.begin()) it=container.begin();it!=container.end();it++)
+#define setbits(x) __builtin_popcountll(n)
+#define zrobits(x) __builtin_ctzll(x)
+
+using namespace std;
 
 typedef long long ll; 
 typedef pair<int, int> pii; 
@@ -23,43 +38,33 @@ const int INF=1e9;
 
 double PI = acos(-1); 
 
-#define fileIO(name) \
-	freopen(name".in", "r", stdin); \
-	freopen(name".out", "w", stdout);
-
-#define FAST_IO  ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define mp make_pair 
-#define pb push_back
-#define eb emplace_back
-#define MOD 1000000007
-#define MAX 1e9
-#define MIN -1e9
-#define PQ(type) priority_queue<type>
-#define PQD(type) priority_queue<type,vector<type>,greater<type> >
-#define ITR :: iterator it
-#define int ll
-#define t int t; cin>>t; while(t--)
-#define TR(container,it) for(typeof(container.begin()) it=container.begin();it!=container.end();it++)
-#define setbits(x) __builtin_popcountll(n)
-#define zrobits(x)      __builtin_ctzll(x)
-void dfs(int x,int y)
-{
-    for(int i=-1;i<2;i++)
-    {
-        for(int j=-1;j<2;j++)
-        {
-            if(i==0 and j==0) continue; //8 way
-            if(i==0 ^ j!=0) continue; // use this for 4 way
-            
-            int nx=x+i, ny=y+j;
-            if(valid(nx,ny))
-            {
-                //code
-                dfs(x+i,y+j);
-            }
-        }
-    }
-}
+void addEdge(vector<int> adj[], int u, int v) 
+{ 
+    adj[u].push_back(v); 
+    adj[v].push_back(u); 
+} 
+  
+// A utility function to do DFS of graph 
+// recursively from a given vertex u. 
+void DFSUtil(int u, vector<int> adj[], 
+                    vector<bool> &visited) 
+{ 
+    visited[u] = true; 
+    cout << u << " "; 
+    for (int i=0; i<adj[u].size(); i++) 
+        if (visited[adj[u][i]] == false) 
+            DFSUtil(adj[u][i], adj, visited); 
+} 
+  
+// This function does DFSUtil() for all  
+// unvisited vertices. 
+void DFS(vector<int> adj[], int V) 
+{ 
+    vector<bool> visited(V, false); 
+    for (int u=0; u<V; u++) 
+        if (visited[u] == false) 
+            DFSUtil(u, adj, visited); 
+} 
 
 void SieveOfEratosthenes(int *pno) {
    for(int i=2;i<1000005;i++){
@@ -79,49 +84,49 @@ template<class T> T lcm(T a, T b){ return (b / gcd(a, b))*a; }
 
 template<typename T> void scan(T &x) 
 { 
-    x = 0; 
-    bool neg = 0; 
-    register T c = getchar(); 
-  
-    if (c == '-') 
-        neg = 1, c = getchar(); 
-  
-    while ((c < 48) || (c > 57)) 
-        c = getchar(); 
-  
-    for ( ; c < 48||c > 57 ; c = getchar()); 
-  
-    for ( ; c > 47 && c < 58; c = getchar() ) 
-        x= (x << 3) + ( x << 1 ) + ( c & 15 ); 
-  
-    if (neg) x *= -1; 
+    x = 0; 
+    bool neg = 0; 
+    register T c = getchar(); 
+  
+    if (c == '-') 
+        neg = 1, c = getchar(); 
+  
+    while ((c < 48) || (c > 57)) 
+        c = getchar(); 
+  
+    for ( ; c < 48||c > 57 ; c = getchar()); 
+  
+    for ( ; c > 47 && c < 58; c = getchar() ) 
+        x= (x << 3) + ( x << 1 ) + ( c & 15 ); 
+  
+    if (neg) x *= -1; 
 } 
-  
+  
 template<typename T> void print(T n) 
 { 
-    bool neg = 0; 
-  
-    if (n < 0) 
-        n *= -1, neg = 1; 
-  
-    char snum[65]; 
-    int i = 0; 
-    do
-    { 
-        snum[i++] = n % 10 + '0'; 
-        n /= 10; 
-    } 
-  
-    while (n); 
-    --i; 
-  
-    if (neg) 
-        putchar('-'); 
-  
-    while (i >= 0) 
-        putchar(snum[i--]); 
-  
-    putchar('\n'); 
+    bool neg = 0; 
+  
+    if (n < 0) 
+        n *= -1, neg = 1; 
+  
+    char snum[65]; 
+    int i = 0; 
+    do
+    { 
+        snum[i++] = n % 10 + '0'; 
+        n /= 10; 
+    } 
+  
+    while (n); 
+    --i; 
+  
+    if (neg) 
+        putchar('-'); 
+  
+    while (i >= 0) 
+        putchar(snum[i--]); 
+  
+    putchar('\n'); 
 } 
 
 
@@ -130,6 +135,12 @@ template<typename T> void print(T n)
 int main(){
 
  //  FAST_IO
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
 
 
 
