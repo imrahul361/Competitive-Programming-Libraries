711e33d794cd5a079ce35bc22c1c26b307aacb3e
Rahul Karmakar <36432937+imrahul361@users.noreply.github.com>
2020-10-04 14:21:40 +0530

    Removing generic print and scan function

--- template.cpp
+++ template.cpp
@@ -93,57 +93,6 @@ void DFS(vector<ll> adj[], ll V)
 template <class T>
 T lcm(T a, T b) { return (b / gcd(a, b)) * a; }
 
-template <typename T>
-void scan(T &x)
-{
-    x = 0;
-    bool neg = 0;
-    register T c = getchar();
-
-    if (c == '-')
-        neg = 1, c = getchar();
-
-    while ((c < 48) || (c > 57))
-        c = getchar();
-
-    for (; c < 48 || c > 57; c = getchar())
-        ;
-
-    for (; c > 47 && c < 58; c = getchar())
-        x = (x << 3) + (x << 1) + (c & 15);
-
-    if (neg)
-        x *= -1;
-}
-
-template <typename T>
-void print(T n)
-{
-    bool neg = 0;
-
-    if (n < 0)
-        n *= -1, neg = 1;
-
-    char snum[65];
-    int i = 0;
-    do
-    {
-        snum[i++] = n % 10 + '0';
-        n /= 10;
-    }
-
-    while (n);
-    --i;
-
-    if (neg)
-        putchar('-');
-
-    while (i >= 0)
-        putchar(snum[i--]);
-
-    putchar('\n');
-}
-
 int main()
 {
 
