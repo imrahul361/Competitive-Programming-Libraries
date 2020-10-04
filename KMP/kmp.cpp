14635e2f9ef91d456b5b7b9a419da453b61f2dbd
Restyled.io <commits@restyled.io>
2020-10-04 14:56:22 +0000

    Restyled by clang-format

--- KMP/kmp.cpp
+++ KMP/kmp.cpp
@@ -1,65 +1,55 @@
-#include <iostream>
 #include <cstring>
+#include <iostream>
 using namespace std;
-void beforeKMP(string pattern, int f[])
-{
-    int m = pattern.length(), k;
-    f[0] = -1;
-    for (int i = 1; i < m; i++)
-    {
-        k = f[i - 1];
-        while (k >= 0)
-        {
-            if (pattern[k] == pattern[i - 1])
-                break;
-            else
-                k = f[k];
-        }
-        f[i] = k + 1;
+void beforeKMP(string pattern, int f[]) {
+  int m = pattern.length(), k;
+  f[0] = -1;
+  for (int i = 1; i < m; i++) {
+    k = f[i - 1];
+    while (k >= 0) {
+      if (pattern[k] == pattern[i - 1])
+        break;
+      else
+        k = f[k];
     }
+    f[i] = k + 1;
+  }
 }
 
-//check whether string contains pattern
-bool KMP(string pattern, string target)
-{
-    int m = pattern.length();
-    int n = target.length();
-    int f[m];
-    beforeKMP(pattern, f);
-    int i = 0;
-    int k = 0;
-    while (i < n)
-    {
-        if (k == -1)
-        {
-            i++;
-            k = 0;
-        }
-        else if (target[i] == pattern[k])
-        {
-            i++;
-            k++;
-            if (k == m)
-                return 1;
-        }
-        else
-            k = f[k];
-    }
-    return 0;
+// check whether string contains pattern
+bool KMP(string pattern, string target) {
+  int m = pattern.length();
+  int n = target.length();
+  int f[m];
+  beforeKMP(pattern, f);
+  int i = 0;
+  int k = 0;
+  while (i < n) {
+    if (k == -1) {
+      i++;
+      k = 0;
+    } else if (target[i] == pattern[k]) {
+      i++;
+      k++;
+      if (k == m)
+        return 1;
+    } else
+      k = f[k];
+  }
+  return 0;
 }
 
-int main()
-{
-    string tar = "demo text pattern";
-    string pat = "patt";
-    if (KMP(pat, tar))
-        cout<<"'"<<pat<<"' found in string '"<<tar<<"'"<<endl;
-    else
-        cout<<"'"<<pat<<"' not found in string '"<<tar<<"'"<<endl;
-    pat = "demo";
-    if (KMP(pat, tar))
-        cout<<"'"<<pat<<"' found in string '"<<tar<<"'"<<endl;
-    else
-        cout<<"'"<<pat<<"' not found in string '"<<tar<<"'"<<endl;
-    return 0;
+int main() {
+  string tar = "demo text pattern";
+  string pat = "patt";
+  if (KMP(pat, tar))
+    cout << "'" << pat << "' found in string '" << tar << "'" << endl;
+  else
+    cout << "'" << pat << "' not found in string '" << tar << "'" << endl;
+  pat = "demo";
+  if (KMP(pat, tar))
+    cout << "'" << pat << "' found in string '" << tar << "'" << endl;
+  else
+    cout << "'" << pat << "' not found in string '" << tar << "'" << endl;
+  return 0;
 }
